c469584297d27382756fb999d132401936a09a0b
Gaara's Laptop <123532482+JustGaara@users.noreply.github.com>
2023-02-02 09:23:40 -0500

    Minor tweaks; fully functional now

--- DotCrossProduct_Feb2/DotCrossProduct_Feb2.py
+++ DotCrossProduct_Feb2/DotCrossProduct_Feb2.py
@@ -18,19 +18,21 @@ cz = (ay*bx) - (ax*by)
 
 magnitude_c = math.sqrt( math.pow(cx,2) + math.pow(cy,2) + math.pow(cz,2) )
 
-angle_cross = (math.asin((magnitude_c)/(magnitude_a * magnitude_b))  * (180/math.pi))
-angle_dot = (math.acos((magnitude_c)/(magnitude_a * magnitude_b))  * (180/math.pi))
+angle_cross = (math.asin((magnitude_c)/(magnitude_a * magnitude_b)))
+angle_dot = (math.acos((magnitude_c)/(magnitude_a * magnitude_b)))
 
-print("The magnitude of vector A is", magnitude_a)
-print("The magnitude of vector B is", magnitude_b)
-print("The magnitude of vector C is", magnitude_c)
+print("The magnitude of vector A is sqrt (",math.pow(ax,2) + math.pow(ay,2) + math.pow(az,2),") =", magnitude_a)
+print("The magnitude of vector B is sqrt (",math.pow(bx,2) + math.pow(by,2) + math.pow(bz,2),") =", magnitude_b)
+print("The magnitude of vector C is sqrt (",math.pow(cx,2) + math.pow(cy,2) + math.pow(cz,2),") =", magnitude_c)
+
+print()
+print("Theta is",((magnitude_c)/(magnitude_a * magnitude_b)))
+print()
+
+print("--- Cross Product ---")
+print("The angle is",angle_cross,"radians and", (angle_cross * (180/math.pi)), "degrees")
 
 print()
 
-print("""--- Cross Product ---
-The angle is""",angle_cross)
-
-print()
-
-print("""--- Dot Product ---
-The angle is""",angle_dot)
+print("--- Dot Product ---")
+print("The angle is",angle_dot,"radians and", (angle_dot * (180/math.pi)), "degrees")
