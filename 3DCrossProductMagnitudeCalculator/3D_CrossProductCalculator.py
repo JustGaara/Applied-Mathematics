24c54e44ec5ffbbdbc71f4fec266cef0383e0cba
Gaara's Laptop <123532482+JustGaara@users.noreply.github.com>
2023-02-07 14:29:09 -0500

    Created 3D Cross product calculator

---
+++ 3DCrossProductMagnitudeCalculator/3D_CrossProductCalculator.py
@@ -0,0 +1,22 @@
+import math
+
+x1 = input("What is the x for the first vector? ")
+
+y1 = input("What is the y for the first vector? ")
+
+z1 = input("What is the z for the first vector? ")
+
+x2 = input("What is the x for the second vector? ")
+
+y2 = input("What is the y for the second vector? ")
+
+z2 = input("What is the z for the second vector? ")
+
+cx = (int(y1)*int(z2)) - (int(y2)*int(z1))
+cy = (int(x1)*int(z2)) - (int(x2)*int(z1))
+cz = (int(x1)*int(y2)) - (int(x2)*int(y1))
+
+magnitude_c = math.sqrt(math.pow(cx, 2) + math.pow(cy, 2) + math.pow(cz, 2))
+
+print("The magnitude of c is", magnitude_c)
+
